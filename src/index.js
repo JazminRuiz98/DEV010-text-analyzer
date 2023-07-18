2c95ea684c053aa1bfbf6b9e972a9322c0e83d33
JazminRuiz98 <aida.jazmin.ruiz.orbe@gmail.com>
2023-07-18 14:44:21 -0600

    boton de borrado funcional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import analyzer from './analyzer.js';
+const textarea = document.getElementsByName("user-input")[0];
+const boton = document.getElementById('reset-button');
 
+boton.addEventListener('click',borrartexto);
+
+function borrartexto () {
+    textarea.value='';
+} 
+
+
+
+
+import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
