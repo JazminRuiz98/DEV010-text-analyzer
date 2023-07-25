ab6df916ab69b830ec35362074bf8b84f02a69c1
JazminRuiz98 <aida.jazmin.ruiz.orbe@gmail.com>
2023-07-25 11:11:21 -0600

    funciones hasta promedio de palabras

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,12 +1,36 @@
+
+
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount:(text) =>{
+    const word = text.trim().split(/\s+/).filter(Boolean);
+    return word.length;
+    /*const textarea = document.querySelector('name="user-input"').value;
+    characternum = text.length;
+
+    const textareadiv = textarea.split("");
+    const wordnum = textareadiv.length;*/
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    /*let contador = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i]===' ') {
+        contador =contador+1;
+      }
+    }
+    console.log(contador);*/
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let countcharacter = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] !==' ') {
+        countcharacter = countcharacter + 1;
+      }
+    }
+    return countcharacter;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-const textarea = document.getElementsByName("user-input")[0];
+/*const textarea = document.getElementsByName("user-input")[0];*/ /*Si se pone [0] con querySelector no funcionara porque inmediatamente detecta al primer elemento del textarea, ya que no es una lista como QuerySelectorAll, por lo tanto no se necesita usar para ingresar al primer elemento*/
+
+const textarea = document.querySelector('textarea[name="user-input"]');
+
 const boton = document.getElementById('reset-button');
 
-boton.addEventListener('click',borrartexto);
+/*function asignartexto (texto){
+  textarea.value=texto;
+}*/
 
+//Para borrar el texto dentro del textarea
 function borrartexto () {
-    textarea.value='';
-} 
+  textarea.value='';
+}
+
+boton.addEventListener('click',borrartexto);
+//boton.addEventListener('click',asignartexto ('Un ejemplo'));
 
 
+
+//Para capturar texto 
+/*function capturatexto() {
+  console.log(textarea.value);
+}
+
+boton.addEventListener('click',capturatexto)*/
+
+//textarea.addEventListener('keyup', (event) =>{
+/*capturametrica.addEventListener('keyup', (event) =>{
+  const metrica = event.target.value;
+  let metricamostrar = '';
+  if( metrica !== ''){
+    metricamostrar = metrica; }
+
+  else {
+    metricamostrar = '0'; }
+
+  document.getElementsById('palabra').innerHTML = metricamostrar;
+});*/
 
 
 import analyzer from './analyzer.js';
