b9dbe97de8853d101074944727772ff79082ac8b
JazminRuiz98 <aida.jazmin.ruiz.orbe@gmail.com>
2023-07-28 10:07:18 -0600

    Funciones de conteo

--- src/analyzer.js
+++ src/analyzer.js
@@ -34,6 +34,10 @@ const analyzer = {
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    /*let averageword = 0;
+    for (let i = 0; i < text.length; i++) {
+      if(text[i] === )
+    }*/
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-/*const textarea = document.getElementsByName("user-input")[0];*/ /*Si se pone [0] con querySelector no funcionara porque inmediatamente detecta al primer elemento del textarea, ya que no es una lista como QuerySelectorAll, por lo tanto no se necesita usar para ingresar al primer elemento*/
+//Archivo JS donde se declaran constantes y funciones para capturar las metricas, el texto en TEXTAREA y funcionalidad del botón para borrar contenido 
+//Declaración de variables para captura de métricas
+//estructura de querySelector para capturar por atributo "document.querySelector('tag[atributo="valor"]');"
+const listWordCount = document.querySelector('li[data-testid="word-count"]');
+const listCharCount = document.querySelector('li[data-testid="character-count"]');
+const listCharNoSpace = document.querySelector('li[data-testid="character-no-spaces-count"]');
+const listNumberCount = document.querySelector('li[data-testid="number-count"]');
+const listNumberSum = document.querySelector('li[data-testid="number-sum"]');
+const listWordAverage = document.querySelector ('li[data-testid="word-length-average"]');
 
+
+
+
+
+
+
+//Captura lo que se ingresa dentro del TEXTAREA 
+//const textarea = document.getElementsByName("user-input")[0];*/ /*Si se pone [0] con querySelector no funcionara porque inmediatamente detecta al primer elemento del textarea, ya que no es una lista como QuerySelectorAll, por lo tanto no se necesita usar para ingresar al primer elemento*/
 const textarea = document.querySelector('textarea[name="user-input"]');
 
-const boton = document.getElementById('reset-button');
+const button = document.getElementById('reset-button');
+
 
 /*function asignartexto (texto){
   textarea.value=texto;
@@ -13,7 +30,7 @@ function borrartexto () {
   textarea.value='';
 }
 
-boton.addEventListener('click',borrartexto);
+button.addEventListener('click',borrartexto);
 //boton.addEventListener('click',asignartexto ('Un ejemplo'));
 
 
@@ -25,18 +42,20 @@ boton.addEventListener('click',borrartexto);
 
 boton.addEventListener('click',capturatexto)*/
 
-//textarea.addEventListener('keyup', (event) =>{
-/*capturametrica.addEventListener('keyup', (event) =>{
-  const metrica = event.target.value;
-  let metricamostrar = '';
-  if( metrica !== ''){
-    metricamostrar = metrica; }
+const metrics = document.querySelector('textarea[name="user-input"]');
 
+metrics.addEventListener('keyup', (event) =>{
+  const texto = event.target.value;
+  console.log(texto);
+  let showMetrics = '';
+  if (texto !== ''){
+    showMetrics = texto;
+  }
   else {
-    metricamostrar = '0'; }
-
-  document.getElementsById('palabra').innerHTML = metricamostrar;
-});*/
+    showMetrics = 'Ingresa un valor';
+  }
+  document.querySelector('li[data-testid="word-count"]').innerHTML = showMetrics;
+});
 
 
 import analyzer from './analyzer.js';
